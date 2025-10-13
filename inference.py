d410112cdec2e35534ab2aacddda532107e2c203
Kishan Kumar Upadhyay <162053085+Kishan4311@users.noreply.github.com>
2025-10-13 19:51:44 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -393,7 +393,7 @@ class InferenceCLI:
 
     def _load_models(self):
         # Target model
-        default_target = "meta-llama/Llama-3.1-3B-Instruct"
+        default_target = "meta-llama/Llama-3.2-3B-Instruct"
         target_quantize = QuantoConfig(weights="int8")  # QuantoConfig(weights="int8")  None
 
         # Drafter model
